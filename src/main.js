c541c3eab32c3ca9a39ecd1ec1e8e6e6f65ac8a0
kinestrik <shadlovskiyy@bk.ru>
2024-07-15 12:42:03 +0300

    d

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import vuelidate from 'vuelidate'
 const app = createApp(App)
 
-app.use(router)
+app.use(router, vuelidate)
 
 app.mount('#app')
